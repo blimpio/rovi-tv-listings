fd0dbfa6fcec3225fc7adf8d4e1828157a5f69ee
Giovanni Collazo <gcollazo@gmail.com>
2012-07-23 00:15:43 -0400

    Completed setup file

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@ from distutils.core import setup
 
 setup(
     name='python-rovi',
-    version='0.1.0',
+    version='0.0.2',
     packages=['rovi'],
     install_requires=[
         'requests >= 0.13.3',
-    ]
+    ],
+
+    author='Giovanni Collazo',
+    author_email='hello@getblimp.com',
+    url='https://github.com/GetBlimp/rovi-tv-listings',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ),
 )
